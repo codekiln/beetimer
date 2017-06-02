aa0c26d33944a2503bb8c8303f4e140fdcccf056
P. Myer Nore <ptr.nore@gmail.com>
2017-06-02 05:28:41 -0400

    add material-components-web example
    * from https://github.com/material-components/material-components-web/tree/master/framework-examples/react

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {PureComponent, PropTypes} from 'react';
 
-class App extends Component {
+import Checkbox from './Checkbox';
+import CheckboxLabel from './CheckboxLabel';
+import FormField from './FormField';
+
+export default class App extends PureComponent {
+  state = {
+    checked: false,
+    disabled: false,
+    indeterminate: false,
+    changeEventCount: 0
+  }
+
   render() {
+    const {checked, disabled, indeterminate, status, changeEventCount} = this.state;
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <main>
+        <h1>MDC-Web Checkbox - React Example</h1>
+        <FormField>
+          <Checkbox id="my-checkbox"
+                    labelId="my-checkbox-label"
+                    disabled={disabled}
+                    indeterminate={indeterminate}
+                    onChange={({target}) => this.setState({
+                      changeEventCount: changeEventCount + 1,
+                      checked: target.checked,
+                      indeterminate: false
+                    })}/>
+          <CheckboxLabel id="my-checkbox-label" for="my-checkbox">
+            The checkbox is currently {this.status()}
+          </CheckboxLabel>
+        </FormField>
+        <div style={{paddingTop: '12px'}}>
+          <button onClick={() => this.setState({indeterminate: true})}>Make Indeterminate</button>
+          <button onClick={() => this.setState({disabled: !disabled})}>Toggle Disabled</button>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <p>{changeEventCount} change events so far</p>
+      </main>
     );
   }
-}
 
-export default App;
+  status() {
+    if (this.state.indeterminate) {
+      return 'indeterminate';
+    }
+    return this.state.checked ? 'checked' : 'unchecked';
+  }
+}
