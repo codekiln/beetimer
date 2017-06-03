6f0f40ba67c4d36b44e3e928ad65c308877d71a4
P. Myer Nore <ptr.nore@gmail.com>
2017-06-03 11:51:29 -0400

    import material ui

--- src/App.js
+++ src/App.js
@@ -1,49 +1,14 @@
-import React, {PureComponent, PropTypes} from 'react';
-
-import Checkbox from './Checkbox';
-import CheckboxLabel from './CheckboxLabel';
-import FormField from './FormField';
+import React, {PureComponent} from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MyAwesomeReactComponent from './MyAwesomeReactComponent';
 
 export default class App extends PureComponent {
-  state = {
-    checked: false,
-    disabled: false,
-    indeterminate: false,
-    changeEventCount: 0
-  }
 
   render() {
-    const {checked, disabled, indeterminate, status, changeEventCount} = this.state;
     return (
-      <main>
-        <h1>MDC-Web Checkbox - React Example</h1>
-        <FormField>
-          <Checkbox id="my-checkbox"
-                    labelId="my-checkbox-label"
-                    disabled={disabled}
-                    indeterminate={indeterminate}
-                    onChange={({target}) => this.setState({
-                      changeEventCount: changeEventCount + 1,
-                      checked: target.checked,
-                      indeterminate: false
-                    })}/>
-          <CheckboxLabel id="my-checkbox-label" for="my-checkbox">
-            The checkbox is currently {this.status()}
-          </CheckboxLabel>
-        </FormField>
-        <div style={{paddingTop: '12px'}}>
-          <button onClick={() => this.setState({indeterminate: true})}>Make Indeterminate</button>
-          <button onClick={() => this.setState({disabled: !disabled})}>Toggle Disabled</button>
-        </div>
-        <p>{changeEventCount} change events so far</p>
-      </main>
+      <MuiThemeProvider>
+        <MyAwesomeReactComponent />
+      </MuiThemeProvider>
     );
-  }
-
-  status() {
-    if (this.state.indeterminate) {
-      return 'indeterminate';
-    }
-    return this.state.checked ? 'checked' : 'unchecked';
   }
 }
--- src/CheckboxLabel.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function CheckboxLabel(props) {
-  const {id, children} = props;
-  const controlId = props['for'];
-  return (
-    <label className="mdc-checkbox-label" id={id} htmlFor={controlId}>{children}</label>
-  );
-}
