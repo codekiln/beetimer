9f62c23b04811bd84dcbb4aed8f1a3be4152c7e0
P. Myer Nore <ptr.nore@gmail.com>
2017-06-09 05:13:53 -0400

    clean up TrackCardView
    * remove dead code
    * break out sections of card in render
    
    start a tracker #10

--- src/Tracks/TrackCardView.js
+++ src/Tracks/TrackCardView.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types";
 import {createStyleSheet, withStyles} from "material-ui/styles";
-import Card, {CardActions, CardContent, CardHeader, CardMedia} from "material-ui/Card";
+import Card, {CardActions, CardContent, CardHeader} from "material-ui/Card";
 import Avatar from "material-ui/Avatar";
 import IconButton from "material-ui/IconButton";
 import Typography from "material-ui/Typography";
@@ -11,15 +11,6 @@ import DeleteIcon from "material-ui-icons/Delete";
 
 const styleSheet = createStyleSheet('TrackCardView', theme => ({
   card: {maxWidth: 400},
-  expand: {
-    transform: 'rotate(0deg)',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)',
-  },
   avatar: {
     // backgroundColor: red[500]
   },
@@ -28,21 +19,11 @@ const styleSheet = createStyleSheet('TrackCardView', theme => ({
     height: 38,
     width: 38,
     color: red[500]
-  },
-  input: {
-    margin: theme.spacing.unit,
-  },
+  }
 }));
 
 
 class TrackCardView extends Component {
-  // state = {
-  //   expanded: false,
-  // };
-  //
-  // handleExpandClick = () => {
-  //   this.setState({expanded: !this.state.expanded});
-  // };
 
   constructor(props) {
     super(props);
@@ -59,27 +40,27 @@ class TrackCardView extends Component {
   render() {
     const
       {classes, name, description} = this.props,
+
       acronym = name
         .split(/\s/)
         .reduce(function (accumulator, word) {
           return accumulator + word.charAt(0).toUpperCase();
-        }, '');
+        }, ''),
 
-    return (
-      <Card className={classes.card}>
+      cardHeader = (
         <CardHeader
           avatar={<Avatar aria-label="Time Tracker" className={classes.avatar}>{acronym}</Avatar>}
           title={name}
-          // subheader="September 14, 2016"
         />
-        <CardMedia>
-          {/*<img src={paellaImage} alt="Contemplative Reptile" />*/}
-        </CardMedia>
+      ),
+
+      cardBody = description ? (
         <CardContent>
-          <Typography component="p">
-            {description}
-          </Typography>
+          <Typography component="p"> { description } </Typography>
         </CardContent>
+      ) : null,
+
+      cardActions = (
         <CardActions disableActionSpacing>
           <IconButton>
             <PlayArrowIcon className={classes.playIcon}/>
@@ -88,20 +69,14 @@ class TrackCardView extends Component {
           <IconButton label="Delete" onClick={this.handleDelete}>
             <DeleteIcon />
           </IconButton>
-          {/*<IconButton*/}
-          {/*className={classnames(classes.expand, {*/}
-          {/*[classes.expandOpen]: this.state.expanded,*/}
-          {/*})}*/}
-          {/*onClick={this.handleExpandClick}*/}
-          {/*>*/}
-          {/*<ExpandMoreIcon />*/}
-          {/*</IconButton>*/}
         </CardActions>
-        {/*<Collapse in={this.state.expanded} transitionDuration="auto" unmountOnExit>*/}
-        {/*<CardContent>*/}
-        {/*<Typography paragraph type="body2">History:</Typography>*/}
-        {/*</CardContent>*/}
-        {/*</Collapse>*/}
+      );
+
+    return (
+      <Card className={classes.card}>
+        {cardHeader}
+        {cardBody}
+        {cardActions}
       </Card>
     );
   }
